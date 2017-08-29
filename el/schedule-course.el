f9171d05b62c89ef1e8027a1c7e8b83b2fc5df88
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2017-08-28 20:04:39 -0400

    New syllabi
    - Add COS 243 syllabus
    - Commit COS 284 addendum for independent study
    - Update material on academic dishonesty (including info from the Provost)
    - Implement Org radio tables for grading table
    - Add more bibliography entries; clean up ISBNs.

--- el/schedule-course.el
+++ el/schedule-course.el
@@ -1,42 +1,28 @@
-(defun org-dblock-write:block-update-time (params)
-  "From org manual A.7"
-  (let ((fmt (or (plist-get params :format) "%d. %m. %Y")))
-    (insert "Last block update at: "
-	    (format-time-string fmt (current-time)))))
 
-(defvar schedule-property-names '("Talk" "Homework"))
-
-(defun visit-org-file (filename)
-  (let ((buffer (find-file-noselect filename)))
-    (message "Visiting file %s" filename)
-    ))
+(defun schedule-item ()
+  (let ((level (org-outline-level))
+		(tags (org-get-tags))
+		(properties (org-entry-properties)))
+	(cond ((member "talk" tags)
+		   (org-schedule nil "2017-12-25"))
+		  (t nil))))
 
 (defun schedule-course ()
   (interactive)
-  (let (outline)
-    (org-map-entries (lambda ()
-		       (let* ((components (org-heading-components))
-			      (level (nth 0 components))
-			      (headline (nth 4 components))
-			      properties)
-			 (dolist (prop-name schedule-property-names)
-			   (let ((val (org-entry-get nil prop-name)))
-			     (when val
-			       (push val properties)
-			       (push prop-name properties)
-			       )))
-			 (push (list level headline properties) outline)))
-		     t 'file)
-    (with-output-to-temp-buffer "*Schedule*"
-      (pp (reverse outline)))
-    ))
+  (pp (org-map-entries #'schedule-item t 'file)))
 
-(with-output-to-temp-buffer "zip"
-    (pp (with-current-buffer "syllabus.org"  (org-element-parse-buffer))))
+;; (defun walk-function ()
+;;   (list (org-outline-level)
+;; 		(org-entry-properties)))
 
-(defun parse-link ()
-  (interactive)
-  (with-output-to-temp-buffer "link"
-    (pp (org-element-link-parser))))
+;; (defun walk-tree ()
+;;   (interactive)
+;;   (pp (org-map-entries 'walk-function t 'tree)))
 
+;; (defun walk-buffer ()
+;;   (interactive)
+;;   (pp (org-map-entries 'walk-function "TAGS=\":talk:\"" 'file)))
 
+;; (defun walk-buffer ()
+;;   (interactive)
+;;   (pp (org-map-entries 'walk-function "TAGS=\":talk:\"" 'file)))
